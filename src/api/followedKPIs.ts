8f9031ac0b109a87bf75cdbb70453e43715099d1
Prashant <160824144+PrashantDalal007@users.noreply.github.com>
2025-06-04 19:58:49 +0530

    Add add-to-following functionality in browse

--- src/api/followedKPIs.ts
+++ src/api/followedKPIs.ts
@@ -2,10 +2,16 @@
 import { MetricCard } from "../types";
 import { metricsData } from "../mock/mockFollowedKPIs";
 
+let followedList: MetricCard[] = [...metricsData];
+
 export const fetchFollowedKPIs = async (): Promise<MetricCard[]> => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(metricsData);
+      resolve(followedList);
     }, 400);
   });
+};
+
+export const addKPIToFollowed = async (metric: MetricCard): Promise<void> => {
+  followedList.push(metric);
 };
